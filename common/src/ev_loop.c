1d5f5fb20a8941e739feae662d735bb6ab317a09
gaailiunas <gailiunaslaurynas@tutanota.com>
2026-02-07 19:50:00 +0200

    feat: basic incomplete event loop

--- common/src/ev_loop.c
+++ common/src/ev_loop.c
@@ -1,1 +1,142 @@
 #include <blazar/common/ev_loop.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+static void list_add(ev_io_t **head, ev_io_t *item)
+{
+    item->next = *head;
+    item->prev = NULL;
+    if (*head) {
+        (*head)->prev = item;
+    }
+    *head = item;
+}
+
+static void list_remove(ev_io_t **head, ev_io_t *item)
+{
+    if (item->prev) {
+        item->prev->next = item->next;
+    }
+    else {
+        *head = item->next;
+    }
+    if (item->next) {
+        item->next->prev = item->prev;
+    }
+    item->next = item->prev = NULL;
+}
+
+ev_loop_t *ev_loop_new(void)
+{
+    ev_loop_t *loop = (ev_loop_t *)malloc(sizeof(*loop));
+    if (!loop) {
+        return NULL;
+    }
+
+    loop->epoll_fd = epoll_create1(0);
+    if (loop->epoll_fd < 0) {
+        free(loop);
+        return NULL;
+    }
+
+    loop->running = false;
+    loop->io_watchers = NULL;
+    loop->user_data = NULL;
+    return loop;
+}
+
+void ev_loop_destroy(ev_loop_t *loop)
+{
+    if (!loop) {
+        return;
+    }
+
+    if (loop->epoll_fd >= 0) {
+        close(loop->epoll_fd);
+    }
+
+    // user owns io_watchers and is responsible for freeing the llist
+    loop->io_watchers = NULL;
+    free(loop);
+}
+
+int ev_loop_run(ev_loop_t *loop)
+{
+    // TODO: to impl
+    return 0;
+}
+
+void ev_loop_stop(ev_loop_t *loop)
+{
+    if (loop) {
+        loop->running = false;
+    }
+}
+
+void ev_io_init(ev_io_t *watcher, ev_io_cb callback, int fd, int events)
+{
+    watcher->fd = fd;
+    watcher->events = events;
+    watcher->callback = callback;
+    watcher->data = NULL;
+    watcher->next = NULL;
+    watcher->prev = NULL;
+}
+
+int ev_io_start(ev_loop_t *loop, ev_io_t *watcher)
+{
+    struct epoll_event ev;
+
+    if (!loop || !watcher) {
+        return -1;
+    }
+
+    ev.events = 0;
+    if (watcher->events & EV_READ) {
+        ev.events |= EPOLLIN;
+    }
+    if (watcher->events & EV_WRITE) {
+        ev.events |= EPOLLOUT;
+    }
+    ev.data.ptr = watcher;
+
+    if (epoll_ctl(loop->epoll_fd, EPOLL_CTL_ADD, watcher->fd, &ev) < 0) {
+        return -1;
+    }
+
+    list_add(&loop->io_watchers, watcher);
+    return 0;
+}
+
+void ev_io_stop(ev_loop_t *loop, ev_io_t *watcher)
+{
+    if (!loop || !watcher) {
+        return;
+    }
+
+    epoll_ctl(loop->epoll_fd, EPOLL_CTL_DEL, watcher->fd, NULL);
+    list_remove(&loop->io_watchers, watcher);
+}
+
+int ev_io_modify(ev_loop_t *loop, ev_io_t *watcher, int events)
+{
+    struct epoll_event ev;
+
+    if (!loop || !watcher) {
+        return -1;
+    }
+
+    watcher->events = events;
+
+    ev.events = 0;
+    if (events & EV_READ) {
+        ev.events |= EPOLLIN;
+    }
+    if (events & EV_WRITE) {
+        ev.events |= EPOLLOUT;
+    }
+    ev.data.ptr = watcher;
+
+    return epoll_ctl(loop->epoll_fd, EPOLL_CTL_MOD, watcher->fd, &ev);
+}
--- include/blazar/common/ev_loop.h
+++ include/blazar/common/ev_loop.h
@@ -1,3 +1,77 @@
 #ifndef BLAZAR_COMMON_EV_LOOP_H
 #define BLAZAR_COMMON_EV_LOOP_H
+
+#include <stdbool.h>
+
+typedef struct ev_loop ev_loop_t;
+typedef struct ev_io ev_io_t;
+
+typedef enum {
+    EV_READ = 0x01,
+    EV_WRITE = 0x02,
+    EV_ERROR = 0x04,
+} ev_event_t;
+
+typedef void (*ev_io_cb)(ev_loop_t *loop, ev_io_t *watcher, int revents);
+
+struct ev_io {
+    int fd;
+    int events;
+    ev_io_cb callback;
+    void *data;
+
+    struct ev_io *next;
+    struct ev_io *prev;
+};
+
+struct ev_loop {
+    bool running;
+    int epoll_fd;
+    ev_io_t *io_watchers;
+    void *user_data;
+};
+
+/**
+ * Create a new event loop
+ * Returns NULL on failure
+ */
+ev_loop_t *ev_loop_new(void);
+
+/**
+ * Destroy event loop and free resources
+ */
+void ev_loop_destroy(ev_loop_t *loop);
+
+/**
+ * Run the event loop
+ * Returns 0 on success, -1 on error
+ */
+int ev_loop_run(ev_loop_t *loop);
+
+/**
+ * Stop the event loop
+ */
+void ev_loop_stop(ev_loop_t *loop);
+
+/**
+ * Initialize an I/O watcher
+ */
+void ev_io_init(ev_io_t *watcher, ev_io_cb callback, int fd, int events);
+
+/**
+ * Start watching for I/O events
+ * Returns 0 on success, -1 on error
+ */
+int ev_io_start(ev_loop_t *loop, ev_io_t *watcher);
+
+/**
+ * Stop watching for I/O events
+ */
+void ev_io_stop(ev_loop_t *loop, ev_io_t *watcher);
+
+/**
+ * Modify events being watched
+ */
+int ev_io_modify(ev_loop_t *loop, ev_io_t *watcher, int events);
+
 #endif // BLAZAR_COMMON_EV_LOOP_H
